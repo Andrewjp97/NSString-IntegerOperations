ab86c538b4b0d0641474a7dcb22abcb575c44342
Andrewjp97 <Andrewjp97@me.com>
2013-09-12 06:18:03 -0400

    Create NSString+IntegerOperations.h

---
+++ NSString+IntegerOperations.h
@@ -0,0 +1,12 @@
+//
+//  NSString+IntegersAsStings.h
+//
+//  Created by Andrew Paterson on 9/8/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (IntegersAsStings)
++ (NSString *)addIntegersStoredAsString:(NSString *)stringOne and:(NSString *)stringTwo;
+
+@end
