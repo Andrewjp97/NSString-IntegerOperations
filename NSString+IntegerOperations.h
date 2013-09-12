09c1e7b8b01edd8bab374adf78b5557b022ba618
Andrewjp97 <Andrewjp97@me.com>
2013-09-12 08:33:06 -0400

    Update NSString+IntegerOperations.h

--- NSString+IntegerOperations.h
+++ NSString+IntegerOperations.h
@@ -7,6 +7,17 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (IntegerOperations)
-+ (NSString *)addIntegersStoredAsString:(NSString *)stringOne and:(NSString *)stringTwo;
++ (NSString *)addString:(NSString *)stringOne and:(NSString *)stringTwo;
+- (void)decrementByOne;
+- (void)incrementByOne;
+- (void)multiplyBy:(NSInteger)multiplier;
+- (void)divideBy:(NSInteger)divisor;
+- (void)modBy:(NSInteger)divisor;
+- (void)addInteger:(NSInteger)additive;
+- (void)subtractInteger:(NSInteger)detractor;
++ (NSString *)multiplyString:(NSString *)stringOne by:(NSString *)stringTwo;
++ (NSString *)divideString:(NSString *)stringOne by:(NSString *)stringTwo;
++ (NSString *)subtractString:(NSString *)stringOne from:(NSString *)stringTwo;
++ (NSString *)modString:(NSString *)stringOne by:(NSString *)stringTwo;
 
 @end
