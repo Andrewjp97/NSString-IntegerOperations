64db150763d3e8ab3e22279f39e05a36aa2287f9
Andrewjp97 <Andrewjp97@me.com>
2013-09-12 08:34:47 -0400

    Update NSString+IntegerOperations.m

--- NSString+IntegerOperations.m
+++ NSString+IntegerOperations.m
@@ -7,7 +7,7 @@
 #import "NSString+IntegersAsStings.h"
 
 @implementation NSString (IntegersAsStings)
-+ (NSString *)addIntegersStoredAsString:(NSString *)stringOne and:(NSString *)stringTwo{
++ (NSString *)addString:(NSString *)stringOne and:(NSString *)stringTwo{
     if ([stringTwo integerValue] != 0 && [stringOne integerValue] != 0) {
         
     NSString *sum = [NSString stringWithFormat:@"%u", [stringOne integerValue] + [stringTwo integerValue]];
@@ -21,4 +21,17 @@
         return nil;
     }
 }
++ (NSString *)addString:(NSString *)stringOne and:(NSString *)stringTwo;
+- (void)decrementByOne;
+- (void)incrementByOne;
+- (void)multiplyBy:(NSInteger)multiplier;
+- (void)divideBy:(NSInteger)divisor;
+- (void)modBy:(NSInteger)divisor;
+- (void)addInteger:(NSInteger)additive;
+- (void)subtractInteger:(NSInteger)detractor;
++ (NSString *)multiplyString:(NSString *)stringOne by:(NSString *)stringTwo;
++ (NSString *)divideString:(NSString *)stringOne by:(NSString *)stringTwo;
++ (NSString *)subtractString:(NSString *)stringOne from:(NSString *)stringTwo;
++ (NSString *)modString:(NSString *)stringOne by:(NSString *)stringTwo;
+
 @end
